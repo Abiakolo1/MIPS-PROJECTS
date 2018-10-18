96b28e238a261c7e666b6b40ebe7974eaae46cc5
ABiakolo1 <aghogho.biakolo@bison.howard.edu>
2018-10-18 17:22:32 -0400

    Gets the next values of my name

--- file.s
+++ file.s
@@ -20,7 +20,7 @@ main:
     syscall
     addi $a0, $a0, 3 #add 3 to 6 to get the value 9
     syscall
-    li $v0, 11
+    li $v0, 11 #allows for characters to be loaded in $a0
     addi $a0, $a0, 1 #add 1 to 9 to get the value 10
     syscall
     addi $a0, $a0, 55 #add 10 to 55 to get the value 65 which is the decimal ascii value of A
@@ -30,6 +30,10 @@ main:
     addi $a0, $a0, 1 #add 1 to 98 to get 104 which is the decimal ascii value of h
     syscall
     addi $a0, $a0, 53 #add 54 to get 104 to get 157 which is the decimal ascii value of o
+    syscall
+    addi $a0, $a0 -54 #add -54 to to 157 to get 103 which is the decimal ascii value of g
+    syscall
+    addi $a0, $a0 1 #add 1 to 103 to get 104 which is the decimal ascii value h
 
 #li $v0, 10 #final code to run code and output the code
 #syscall #output and exit program
