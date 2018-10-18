04ed628c42abf93eee5131be917fe896f0c93b71
ABiakolo1 <aghogho.biakolo@bison.howard.edu>
2018-10-18 18:00:22 -0400

    Got the commented value of the first letter of my surname.

--- file.s
+++ file.s
@@ -23,24 +23,25 @@ main:
     li $v0, 11 #allows for characters to be loaded in $a0
     addi $a0, $a0, 1 #add 1 to 9 to get the value 10
     syscall
-    addi $a0, $a0, 55 #add 10 to 55 to get the value 65 which is the decimal ascii value of A
+    addi $a0, $a0, 56 #add 56 to 10 to get 66, the ascii value of B
     syscall
-    addi $a0, $a0, 38 #add 33 to 65 get 103 which is the decimal ascii value of g
-    syscall
-    addi $a0, $a0, 1 #add 1 to 98 to get 104 which is the decimal ascii value of h
-    syscall
-    addi $a0, $a0, 53 #add 54 to get 104 to get 157 which is the decimal ascii value of o
-    syscall
-    addi $a0, $a0 -54 #add -54 to to 157 to get 103 which is the decimal ascii value of g
-    syscall
-    addi $a0, $a0 1 #add 1 to 103 to get 104 which is the decimal ascii value h *
-    syscall
-    addi $a0, $a0, 53 #add 53 to 104 to get 157, the decimal value of o
-    syscall
-    addi $a0, $a0, -125 #add -125 to 157 to get the decimal value of space
-    syscall
-#li $v0, 10 #final code to run code and output the code
-#syscall #output and exit program
+
+#addi $a0, $a0, 33 #add 33 to 32 to get the value 65 which is the decimal ascii value of A
+#   syscall
+#   addi $a0, $a0, 38 #add 33 to 65 get 103 which is the decimal ascii value of g
+#   syscall
+#   addi $a0, $a0, 1 #add 1 to 98 to get 104 which is the decimal ascii value of h
+#   syscall
+#   addi $a0, $a0, 7 #add 54 to get 104 to get 157 which is the decimal ascii value of o
+#   syscall
+#   addi $a0, $a0 -8 #add -54 to to 157 to get 103 which is the decimal ascii value of g
+#   syscall
+#   addi $a0, $a0 1 #add 1 to 103 to get 104 which is the decimal ascii value h *
+#   syscall
+#   addi $a0, $a0, 7 #add 53 to 104 to get 157, the decimal value of o
+#   syscall
+#   li $v0, 10 #final code to run code and output the code
+#   syscall #output and exit program
 
 
 
